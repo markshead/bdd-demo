43ffa42a85c9432f8c77bfae303e7dec64dc671d
Mark Shead <markshead@mws-air.local>
2016-10-24 11:54:10 -0500

    Switch to Chrome for Selenium

--- src/test/java/net/xeric/demos/TestConfig.java
+++ src/test/java/net/xeric/demos/TestConfig.java
@@ -2,6 +2,8 @@ package net.xeric.demos;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +15,9 @@ public class TestConfig {
 
     @Bean(destroyMethod = "quit")
     public WebDriver getWebDriver() {
-        final WebDriver webDriver = new FirefoxDriver();
+        //final WebDriver webDriver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/bin/mac/chromedriver");
+        final WebDriver webDriver = new ChromeDriver();
         return webDriver;
     }
 }
